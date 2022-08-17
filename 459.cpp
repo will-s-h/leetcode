a94d50ba9b8def6d0fb9708ba7a836336807f9eb
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-16 18:27:45 -0700

    Update 459.cpp

--- 459.cpp
+++ 459.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s){ //O(n), using KMP
+    bool repeatedSubstringPattern(string s){ //O(n), using KMP. runtime: 23 ms
         int n = (int) s.length();
         vector<int> dp(n);
         for(int i = 1; i < n; i++){
@@ -11,7 +11,7 @@ public:
         return dp[n-1] && (n%(n-dp[n-1]) == 0);
     }
     
-    bool slow(string s) { //O(n sigma(n)), where sigma(n) is the divisor function.
+    bool slow(string s) { //O(n sigma(n)), where sigma(n) is the divisor function. runtime: 36 ms (actually not that bad)
         //see [https://math.stackexchange.com/questions/3043181/growth-rate-of-a-divisor-function] for details on the divisor function's growth.
         for(int l = 1; l < s.length()/2 + 1; l++){
             if(s.length() % l != 0) continue;
