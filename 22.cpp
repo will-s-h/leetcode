1f341676e96315be39714ea32e407979c9b635d1
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-17 00:05:45 -0700

    Update 22.cpp

--- 22.cpp
+++ 22.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    vector<string> generateParenthesis(int n) { //O(4^n / √n), nth catalan number * n, 8 ms
+        vector<string> res;
+        addingpar(res, "", n, 0);
+        return res;
+    }
+    
+    void addingpar(vector<string> &v, string str, int n, int m){
+        if(n==0 && m==0) {
+            v.push_back(str);
+            return;
+        }
+        if(m > 0){ addingpar(v, str+")", n, m-1); }
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+    }
+    
     vector<string> slow(int n) { //O(n * n!), 30 ms
         vector<int> a; for(int i = 1; i <= n; i++) a.push_back(i);
         vector<string> ans; unordered_map<string, bool> seen;
