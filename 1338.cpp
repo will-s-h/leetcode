c9bfe37e2eaaff7dc6373050210b38285526d36e
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-20 00:21:45 -0700

    Create 1338.cpp

---
+++ 1338.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) { //O(N log N)
+        unordered_map<int, int> m;
+        for(int i = 0; i < arr.size(); i++){
+            m[arr[i]]++;
+        }
+        
+        auto cmp = [](pair<int,int>& p1, pair<int,int>& p2){
+            return p1.second > p2.second;
+        };
+        
+        vector<pair<int,int>> v(m.begin(), m.end());
+        sort(v.begin(), v.end(), cmp);
+        
+        int sum = 0; int num = 0;
+        for(pair<int,int> p : v){
+            sum += p.second; num++;
+            if(sum >= (arr.size()+1)/2) return num;
+        }
+        return num;
+    }
+};
