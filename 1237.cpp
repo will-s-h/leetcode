c2723300196dd93ba97bae2c9059987d72a30db6
will-s-h <107830908+will-s-h@users.noreply.github.com>
2022-08-19 23:37:58 -0700

    Update 1237.cpp

--- 1237.cpp
+++ 1237.cpp
@@ -12,69 +12,13 @@
 
 class Solution {
 public:
-    
-    vector<vector<int>> sol;
-    vector<int> seenx;
-    
-    bool findy(CustomFunction& c, int x, int z){
-        if(seenx[x]) return (seenx[x] > 0);
-        int loy = 1;
-        int hiy = 1000;
-        int lobound = 1001;
-        
-        //find the lower bound
-        while(loy <= hiy){
-            int mid = loy + (hiy - loy) / 2;
-            int val = c.f(x, mid);
-            if(val >= z){
-                lobound = mid;
-                hiy = mid - 1;
-            } else {
-                loy = mid + 1;
-            }
-        }
-        
-        if(lobound == 1001) {
-            seenx[x] = -2; //all values are less
-            return false;
-        } else if(lobound == 1 && c.f(x, 1) > z){
-            seenx[x] = -1; //all values are greater
-            return false;
-        }
-        
-        //find the upper bound
-        loy = lobound;
-        hiy = 1000;
-        int hibound = lobound;
-        while(loy <= hiy){
-            int mid = loy + (hiy - loy) / 2;
-            int val = c.f(x, mid);
-            if(val > z){
-                hiy = mid - 1;
-            } else{
-                hibound = mid;
-                loy = mid + 1;
-            }
-        }
-        
-        for(int i = lobound; i <= hibound; i++){
-            if(c.f(x, i) == z) sol.push_back({x, i});
-        }
-        
-        seenx[x] = 1;
-        return true;
-    }
-    
-    vector<vector<int>> findSolution(CustomFunction& c, int z) { //quite slow, O(N^2 log N), but still passes
-        sol = vector<vector<int>>();
-        seenx = vector<int>(1001);
-        int lox = 1;
-        int hix = 1000;
-        
-        for(int i = lox; i <= hix; i++){
-            findy(c, i, z);
-        }
-        
+    vector<vector<int>> findSolution(CustomFunction& c, int z) { //O(x+y)
+        vector<vector<int>> sol;
+        int y = 1000;
+        for(int x = 1; x <= 1000; x++){
+            while (y > 1 && c.f(x,y) > z) y--;
+            if(c.f(x,y) == z) sol.push_back({x,y});
+        } 
         return sol;
     }
 };
